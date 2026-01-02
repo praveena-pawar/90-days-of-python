4e78213788745ca151fca266dd0199acc30c0207
praveena-pawar <praveena.dev02@gmail.com>
2026-01-02 10:50:10 +0530

    Count even and odd numbers

---
+++ day02_count_with_else.py
@@ -0,0 +1,13 @@
+# 1. Count even and odd numbers
+nums = [1, 2, 3, 4, 5, 6]
+even_count = 0
+odd_count = 0
+
+for n in nums:
+    if n % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even:", even_count)
+print("Odd:", odd_count)
