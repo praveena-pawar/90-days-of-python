9071ae25794c7243d0718eae2c45a67db88180b9
praveena-pawar <praveena.dev02@gmail.com>
2026-01-04 14:41:22 +0530

    Think: and answer your self

--- day04_if_elif_else.py
+++ day04_if_elif_else.py
@@ -8,3 +8,14 @@ elif num == 0:
 else:
     print("Negative")
 
+# Think:
+
+# Why only one print runs?
+# In an if–elif–else chain, Python checks conditions top to bottom.
+# The first condition that becomes True runs.
+# After that, Python skips the rest completely.
+
+# Why order matters (> 0 first)?
+# Python checks conditions in the order we write them
+# If we write the wrong order, the logic can break
+
