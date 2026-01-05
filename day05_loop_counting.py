32294a14f26af043a88313172b69d6e90cbd766f
praveena-pawar <praveena.dev02@gmail.com>
2026-01-05 08:37:52 +0530

    Read & think

---
+++ day05_loop_counting.py
@@ -0,0 +1,12 @@
+# 1. Read & think
+nums = [2, 5, 8, 11, 14]
+even_count = 0
+odd_count = 0
+
+for n in nums:
+    if n % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print(even_count, odd_count)
