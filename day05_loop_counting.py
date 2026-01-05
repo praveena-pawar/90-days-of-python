600026025aaa6def5e1ffcb93d64eabb83916efb
praveena-pawar <praveena.dev02@gmail.com>
2026-01-05 08:40:44 +0530

    Think before writting the code

--- day05_loop_counting.py
+++ day05_loop_counting.py
@@ -10,3 +10,11 @@ for n in nums:
         odd_count += 1
 
 print(even_count, odd_count)
+
+# Think:
+
+# Why do we need two counters?
+# We use two counters because we are tracking two different results.
+
+# What happens in each loop iteration?
+# A for loop goes one element at a time.
