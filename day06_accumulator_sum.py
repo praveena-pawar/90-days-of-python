40ac3133d3e895f2464808869f783d959c429b24
praveena-pawar <praveena.dev02@gmail.com>
2026-01-06 11:02:33 +0530

    Think and answer

--- day06_accumulator_sum.py
+++ day06_accumulator_sum.py
@@ -6,3 +6,12 @@ for n in nums:
     total += n
 
 print(total)
+
+
+# Think:
+
+# Why did we start total = 0?
+# We start with total = 0 because 0 is the neutral value for addition.
+
+# What happens in each loop iteration?
+# In each iteration the loop adds the current element to the accumulator.
