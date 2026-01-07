5fb3515c92f54e60f60c13e875d455a3cfcc0f16
praveena-pawar <praveena.dev02@gmail.com>
2026-01-07 19:11:57 +0530

    answer for the three questions

--- day07_functions_basics.py
+++ day07_functions_basics.py
@@ -9,7 +9,15 @@ print(result)
 # Think:
 
 # Why do we use def?
+# def is used to define (create) a function.
 
 # What does return do?
+# return sends the result back to the place where the function was called.
 
 # What happens after return?
+# The function stops immediately
+# Control goes back to the caller Any code after return inside the function does not run
+# Example : 
+# def test():
+#     return 10
+#     print("Hello")  # ❌ never runs
