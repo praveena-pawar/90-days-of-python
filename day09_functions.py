78a5f3c601926ce5f57b5a97378763bbb7b252e5
praveena-pawar <praveena.dev02@gmail.com>
2026-01-09 20:18:24 +0530

    soved task 2

--- day09_functions.py
+++ day09_functions.py
@@ -27,5 +27,16 @@ print("The number is :", result)
 # Counts how many numbers are even
 # Returns the count
 
+nums = [2, 5, 8, 10, 13]
 
+def count_even(nums):
+    even_count = 0
+    for n in nums:
+        if n % 2 == 0:
+            even_count += 1
+
+    return even_count
+        
+result = count_even(nums)
+print("The total even numbers are :", result)
 
