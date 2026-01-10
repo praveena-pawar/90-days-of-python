3a1d598ff5276a5bee4db4c987a0d97dea7fbcc5
praveena-pawar <praveena.dev02@gmail.com>
2026-01-10 12:23:14 +0530

    Function that RETURNS a LIST

---
+++ day10_list_filtering.py
@@ -0,0 +1,9 @@
+# Concept 1: Function that RETURNS a LIST
+# Until now, you returned numbers/strings.
+# Today: return a list.
+
+# Example idea (not code):
+
+# Input list → [5, 12, 7, 20]
+
+# Output list → [12, 20] (numbers > 10)
