8dc9a3f64e895022feb420c08346c89c5efe4cfc
praveena-pawar <praveena.dev02@gmail.com>
2026-01-10 12:33:09 +0530

    sloved Task 1

--- day10_list_filtering.py
+++ day10_list_filtering.py
@@ -14,3 +14,34 @@
 # Write a function that:
 # Takes a list of numbers
 # Returns a new list of only EVEN numbers
+nums = [1, 4, 7, 10, 3]
+
+def get_even_numbers(nums):
+    even_num = []
+    for n in nums:
+        if n % 2 == 0:
+            even_num.append(n)
+
+    return even_num
+
+result = get_even_numbers(nums)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+# nums = [1, 4, 7, 10, 3]
+# even = []
+
+# for n in nums:
+#     if n % 2 == 0:
+#         even.append(n)
+# print(even)
