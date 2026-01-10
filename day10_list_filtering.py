f15007b02d89235da2bf97c92131e0b0394a3ab7
praveena-pawar <praveena.dev02@gmail.com>
2026-01-10 12:24:24 +0530

    Takes a list of numbers and Returns a new list of only EVEN numbers

--- day10_list_filtering.py
+++ day10_list_filtering.py
@@ -1,4 +1,4 @@
-# Concept 1: Function that RETURNS a LIST
+# Concept : Function that RETURNS a LIST
 # Until now, you returned numbers/strings.
 # Today: return a list.
 
@@ -7,3 +7,10 @@
 # Input list → [5, 12, 7, 20]
 
 # Output list → [12, 20] (numbers > 10)
+
+
+# Task: 1
+
+# Write a function that:
+# Takes a list of numbers
+# Returns a new list of only EVEN numbers
