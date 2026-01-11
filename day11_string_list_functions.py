4faf05e35f0f7cb07aac89a6c95f6fba8f7f6b9c
praveena-pawar <praveena.dev02@gmail.com>
2026-01-11 09:04:21 +0530

    Topic: Functions with LIST + STRING + LOGIC (Mixed Problems)

---
+++ day11_string_list_functions.py
@@ -0,0 +1,5 @@
+# Topic: Functions with LIST + STRING + LOGIC (Mixed Problems)
+# By the end of today, you’ll be comfortable with:
+# 1️ Processing lists of strings
+# 2️ Using functions that return counts & lists
+# 3️ Writing clean, interview-style logic
