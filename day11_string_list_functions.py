49456e9dac6ae3f85c2d9bcdbfd68ce1d73a110b
praveena-pawar <praveena.dev02@gmail.com>
2026-01-11 09:08:12 +0530

    Task 1 : Counts how many words have length greater than 5

--- day11_string_list_functions.py
+++ day11_string_list_functions.py
@@ -3,3 +3,11 @@
 # 1️ Processing lists of strings
 # 2️ Using functions that return counts & lists
 # 3️ Writing clean, interview-style logic
+
+
+# Task : 1
+
+# Write a function that:
+# Takes a list of words
+# Counts how many words have length greater than 5
+# Returns the count
