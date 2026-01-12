c41d9d051912625ea2ade6a9d4aabcbfe023f500
praveena-pawar <praveena.dev02@gmail.com>
2026-01-12 11:25:31 +0530

    Task 1

--- day12_counters_with_function.py
+++ day12_counters_with_function.py
@@ -7,3 +7,13 @@
 # Using index while looping
 
 # String + List logic together
+
+
+
+#  Task : 1
+
+# Given a list of numbers, write a function that:
+# Counts how many are positive
+# Counts how many are negativ
+# Counts how many are zero
+# Return all three values.
