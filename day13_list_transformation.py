ac7a08376dee9e8b00e3dfc8dc68f00a837eee3d
praveena-pawar <praveena.dev02@gmail.com>
2026-01-13 21:26:34 +0530

    solved Task 1

--- day13_list_transformation.py
+++ day13_list_transformation.py
@@ -6,3 +6,21 @@
 # Stores squares of only positive numbers
 # Ignores zero and negative numbers
 # Returns the new list
+
+nums = [2, -3, 4, 0, -1, 5, 6]
+
+def squares_of_positive_num(nums):
+    positive_num = []
+    for n in nums:
+        if n < 0:
+            continue
+        elif n == 0:
+            continue
+        else:
+            positive_num.append(n * n) 
+            
+
+    return positive_num
+
+result = squares_of_positive_num(nums)
+print(result)
