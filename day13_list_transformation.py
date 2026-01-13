4a16f2979f709d26edcb599bc5dfd84578d5c608
praveena-pawar <praveena.dev02@gmail.com>
2026-01-13 21:09:18 +0530

    Task 1

---
+++ day13_list_transformation.py
@@ -0,0 +1,8 @@
+# Task 1 :
+
+# You are given a list of numbers.
+# Write a function that:
+# Creates a new list
+# Stores squares of only positive numbers
+# Ignores zero and negative numbers
+# Returns the new list
