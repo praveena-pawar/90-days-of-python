fc2d8db95d014a88c369f22feaf361b8cefef0ef
praveena-pawar <praveena.dev02@gmail.com>
2026-01-15 11:07:28 +0530

    solved task 1

--- day15_dict_list_logic.py
+++ day15_dict_list_logic.py
@@ -5,3 +5,25 @@
 # Creates a character frequency dictionary
 # Returns a list of characters that appear more than once
 # Ignore spaces
+
+text = "programming"
+
+def repeated_characters(text):
+    freq = {}
+
+    for ch in text:
+        if ch == " ":
+            continue
+        if ch in freq:
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+
+    result = []
+    for ch in freq:
+        if freq[ch] > 1:
+            result.append(ch)
+
+    return result
+
+print(repeated_characters(text))
