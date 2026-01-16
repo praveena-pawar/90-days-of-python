7ac486d95cabf59496bcb629c977475ea8b29bff
praveena-pawar <praveena.dev02@gmail.com>
2026-01-16 20:53:42 +0530

    solved Task 1

--- day16_safe_dictionary.py
+++ day16_safe_dictionary.py
@@ -6,3 +6,16 @@
 # Uses dict.get()
 # Ignores spaces
 # Returns the dictionary
+
+word = "apple"
+
+def character_frequency(word):
+    freq = {}
+
+    for ch in word:
+        if ch != " ":                 
+            freq[ch] = freq.get(ch, 0) + 1
+
+    return freq
+
+print(character_frequency("apple"))
