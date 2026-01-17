a9eab14692ffb4ceb8f7cfd3082e7c36a5e1c394
praveena-pawar <praveena.dev02@gmail.com>
2026-01-17 13:41:51 +0530

    solved task 1

--- day17_dict_list_grouping.py
+++ day17_dict_list_grouping.py
@@ -3,3 +3,19 @@
 # Write a function that:
 # Takes a list of numbers
 # Groups them into a dictionary:
+
+nums = [1, 2, 3, 4, 5]
+
+def group_even_odd(nums):
+    result = {"even": [], "odd": []}
+
+    for num in nums:
+        if num % 2 == 0:
+            result["even"].append(num)
+        else:
+            result["odd"].append(num)
+
+    return result
+
+print(group_even_odd(nums))
+    
