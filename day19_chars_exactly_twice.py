6524b2bb8ed0d2aa4f30fe85e2306ae2fe24d72c
praveena-pawar <praveena.dev02@gmail.com>
2026-01-19 22:19:29 +0530

    solved task 1

--- day19_chars_exactly_twice.py
+++ day19_chars_exactly_twice.py
@@ -4,3 +4,22 @@
 # Takes a string
 # Counts frequency of each character (ignore spaces)
 # Returns a list of characters that appear exactly twice
+
+text = "success"
+
+def characters_appear_twice(text):
+    freq = {}
+
+    for ch in text:
+        if ch == " ":
+            continue
+        freq[ch] = freq.get(ch, 0) + 1
+
+    result = []
+    for ch in freq:
+        if freq[ch] == 2:
+            result.append(ch)
+
+    return result
+
+print(characters_appear_twice(text))
