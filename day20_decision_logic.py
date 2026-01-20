cdaaa22f5ba8f303a1bb5cd85fb0cacd602af02e
praveena-pawar <praveena.dev02@gmail.com>
2026-01-20 23:16:22 +0530

    solved task 1

--- day20_decision_logic.py
+++ day20_decision_logic.py
@@ -8,3 +8,29 @@
 # zero
 # Returns which category has the maximum count as a string:
 # "positive", "negative", or "zero"
+
+nums = [1, -2, 0, 3, 0, -1, 0]
+
+def max_category(nums):
+    counts = {"positive": 0, "negative": 0, "zero": 0}
+
+    for n in nums:
+        if n > 0:
+            counts["positive"] += 1
+        elif n < 0:
+            counts["negative"] += 1
+        else:
+            counts["zero"] += 1
+
+    max_count = 0
+    max_category = ""
+
+    for category in counts:
+        if counts[category] > max_count:
+            max_count = counts[category]
+            max_category = category
+
+    return max_category
+
+
+print(max_category(nums))
