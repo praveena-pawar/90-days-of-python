817ca4ecdcb3ed66cfe340def6ac3f0ee1f0b6c2
praveena-pawar <praveena.dev02@gmail.com>
2026-01-20 23:06:38 +0530

    Task 1

---
+++ day20_decision_logic.py
@@ -0,0 +1,10 @@
+# Task 1 :
+
+# Write a function that:
+# Takes a list of integers
+# Counts how many are:
+# positive
+# negative
+# zero
+# Returns which category has the maximum count as a string:
+# "positive", "negative", or "zero"
