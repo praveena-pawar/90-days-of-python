d6b5aeedc2b0733ff230f84a8bbe7bf620a5f576
praveena-pawar <praveena.dev02@gmail.com>
2026-01-21 22:22:46 +0530

    solved problem

--- day21_light_practice.py
+++ day21_light_practice.py
@@ -3,3 +3,14 @@
 # Write a function that:
 # Takes a list of numbers
 # Returns how many numbers are greater than 10
+
+nums = [5, 12, 3, 18, 7, 25]
+
+def count_greater_than_10(nums):
+    count = 0
+    for num in nums:
+        if num > 10:
+            count += 1
+    return count
+
+print(count_greater_than_10(nums))
