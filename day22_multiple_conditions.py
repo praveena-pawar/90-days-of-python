165523b3ac264b5ed9ae84d07a22070554295872
praveena-pawar <praveena.dev02@gmail.com>
2026-01-22 22:01:27 +0530

    solved task 1

--- day22_multiple_conditions.py
+++ day22_multiple_conditions.py
@@ -6,3 +6,18 @@
 # numbers greater than 0 and even
 # numbers less than 0 and odd
 # Returns the counts as a dictionary
+
+nums = [2, -3, 4, -5, 0, 7, -8]
+
+def count_numbers(nums):
+    result = {"positive_even": 0,"negative_odd": 0}
+
+    for n in nums:
+        if n > 0 and n % 2 == 0:
+            result["positive_even"] += 1
+        elif n < 0 and n % 2 != 0:
+            result["negative_odd"] += 1
+
+    return result
+
+print(count_numbers(nums))
