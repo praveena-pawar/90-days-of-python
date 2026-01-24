04509efed3bbb3eb32456d8a69aaa0d20d8ab305
praveena-pawar <praveena.dev02@gmail.com>
2026-01-24 10:59:02 +0530

    task 2 :  Count how many vowels and consonants are in a string.

--- day24_string_character_frequency.py
+++ day24_string_character_frequency.py
@@ -26,3 +26,10 @@ print(chr_frequency(s))
 
 #output:
 #{'d': 1, 'a': 2, 't': 1, 's': 1, 'c': 2, 'i': 1, 'e': 2, 'n': 1}
+
+
+
+
+# Task 2 :
+
+# Count how many vowels and consonants are in a string.
