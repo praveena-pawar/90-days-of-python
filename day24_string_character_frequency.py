8feccdc8f5df7ccbf19468b891130ee790c8bf46
praveena-pawar <praveena.dev02@gmail.com>
2026-01-24 10:54:52 +0530

    solved task 1

--- day24_string_character_frequency.py
+++ day24_string_character_frequency.py
@@ -9,3 +9,17 @@
 
 #s = "datascience"
 # Count how many times each character appears in a string.
+
+s = "datascience"
+
+def chr_frequency(s):
+    freq = {}
+    for ch in s:
+        if ch in freq:
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+    
+    return freq
+
+print(chr_frequency(s))
