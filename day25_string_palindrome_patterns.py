f1cef2a9e316b339e3475569b165f9cca7ef5465
praveena-pawar <praveena.dev02@gmail.com>
2026-01-25 20:29:53 +0530

    solved task 1

--- day25_string_palindrome_patterns.py
+++ day25_string_palindrome_patterns.py
@@ -10,3 +10,29 @@
 # Task 1 :
 
 # Check whether a given string is a palindrome.
+
+s = "madam"
+
+def is_palindrome(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+
+        if not s[left].isalpha():
+            left += 1
+            continue
+
+        if not s[right].isalpha():
+            right -= 1
+            continue
+
+        if s[left].lower() != s[right].lower():
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+print(is_palindrome(s))
