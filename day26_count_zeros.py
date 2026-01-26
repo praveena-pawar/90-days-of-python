08d28658f55ed9101e1a571a8cb0cf3e3a2f49d4
praveena-pawar <praveena.dev02@gmail.com>
2026-01-26 21:30:17 +0530

    task 1

---
+++ day26_count_zeros.py
@@ -0,0 +1,13 @@
+# Problem: Count Zeros in a List
+
+nums = [1, 0, 2, 0, 3, 0, 4]
+
+def count_zeros(nums):
+    zeros = 0
+    for n in nums:
+        if n == 0:
+            zeros += 1
+
+    return zeros
+
+print(count_zeros(nums))
