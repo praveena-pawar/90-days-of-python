5141d5c252cc277c429af1efd999922bb0e1d913
praveena-pawar <praveena.dev02@gmail.com>
2026-01-27 17:22:16 +0530

    solved Task 1

--- day27_array_basic_logic_patterns.py
+++ day27_array_basic_logic_patterns.py
@@ -2,3 +2,15 @@
 
 # Find Maximum Element
 
+nums = [3, 7, 2, 9, 4]
+
+def find_max(nums):
+    max_num = nums[0]
+    for n in nums:
+        if n > max_num:
+            max_num = n
+    
+    return max_num
+
+print(find_max(nums))
+
