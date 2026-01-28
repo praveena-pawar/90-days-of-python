17aaf77511814312784dd75ffdc32abab93ee7b5
praveena-pawar <praveena.dev02@gmail.com>
2026-01-28 17:54:13 +0530

    solved task 1

--- day28_array_tracking_logic_patterns.py
+++ day28_array_tracking_logic_patterns.py
@@ -1,3 +1,19 @@
 # Task 1 :
 
 # Given a list of numbers, find the second largest number.
+
+def second_largest(nums):
+    largest = nums[0]
+    second_largest = float('-inf')
+
+    for n in nums:
+        if n > largest:
+            second_largest = largest
+            largest = n
+        elif n < largest and n > second_largest:
+            second_largest = n
+
+    return second_largest
+
+
+print(second_largest([3, 7, 2, 9, 4]))
