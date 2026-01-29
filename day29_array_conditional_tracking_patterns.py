edafd6531984f5ec60f9355210c5d33949b9b7eb
praveena-pawar <praveena.dev02@gmail.com>
2026-01-29 18:47:43 +0530

    solved task 1

--- day29_array_conditional_tracking_patterns.py
+++ day29_array_conditional_tracking_patterns.py
@@ -1,3 +1,15 @@
 # Task 1 :
 
 # Given a list of numbers, find the largest even number.
+
+def largest_even(nums):
+    even_nums = None
+    for n in nums:
+        if n % 2 == 0:
+            if even_nums is None or n > even_nums:
+                even_nums = n
+            
+
+    return even_nums
+
+print(largest_even([3, 7, 2, 9, 10, 4, 5]))
