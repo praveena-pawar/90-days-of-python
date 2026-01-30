ba761eacba4340767529b434adf7475e55ef843f
praveena-pawar <praveena.dev02@gmail.com>
2026-01-30 21:50:32 +0530

    solved task 1

--- day30_array_largest_less_than_k.py
+++ day30_array_largest_less_than_k.py
@@ -1,3 +1,16 @@
 # Task 1 :
 
 # Given a list and a value k, find the largest number strictly less than k.
+
+def largest_less_than_k(nums, k):
+    best = None
+
+    for n in nums:
+        if n < k:
+            if best is None or n > best:
+                best = n
+
+    return best
+
+
+print(largest_less_than_k( [3, 7, 2, 9, 4, 10, 5], 8))
