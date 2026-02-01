0d8bedf0bc96bd66dcbd06d4ec57cea7308da261
praveena-pawar <praveena.dev02@gmail.com>
2026-02-01 23:34:20 +0530

    solved task 1

--- day32_array_consecutive_streak_patterns.py
+++ day32_array_consecutive_streak_patterns.py
@@ -1,3 +1,21 @@
 # Task 1 :
 
 # Find the length of the longest consecutive increasing streak.
+
+def longest_increasing_streak(nums):
+    current = 1
+    max_streak = 1
+
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i - 1]:
+            current += 1
+        else:
+            current = 1
+
+        if current > max_streak:
+            max_streak = current
+
+    return max_streak
+
+
+print(longest_increasing_streak([1, 3, 2, 4, 6, 5, 7]))
