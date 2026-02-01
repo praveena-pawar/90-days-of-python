a5f7d0dba009a4a150e5f572219445f7b9c31ccf
praveena-pawar <praveena.dev02@gmail.com>
2026-02-01 23:27:33 +0530

    solved task 1

--- day31_array_adjacent_increase_pattern.py
+++ day31_array_adjacent_increase_pattern.py
@@ -1,3 +1,12 @@
 # Task 1 :
 
 # Count how many times the array goes up.
+
+def count_ups(arr):
+    count = 0
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            count += 1
+    return count
+
+print(count_ups([1, 3, 2, 4, 6, 5]))
