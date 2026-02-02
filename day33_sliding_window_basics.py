b9475df4d3667e0ec18e229d08c3997b98485fd8
praveena-pawar <praveena.dev02@gmail.com>
2026-02-02 17:48:19 +0530

    solve dtask 1

--- day33_sliding_window_basics.py
+++ day33_sliding_window_basics.py
@@ -3,3 +3,18 @@
 # Find maximum sum of any subarray of size k.
 # nums = [2, 1, 5, 1, 3, 2]
 # k = 3
+
+def max_sum(nums, k):
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+
+    for i in range(k, len(nums)):
+        window_sum += nums[i]        # add new element
+        window_sum -= nums[i - k]    # remove old element
+        max_sum = max(max_sum, window_sum)
+
+    return max_sum
+
+
+nums = [2, 1, 5, 1, 3, 2]
+print(max_sum(nums, 3))
