7ea1a10142bf5f0343e98b8826ad9d8d0ad675d0
praveena-pawar <praveena.dev02@gmail.com>
2026-02-03 22:50:00 +0530

    solved task 1

--- day34_sliding_window_strings.py
+++ day34_sliding_window_strings.py
@@ -1,3 +1,22 @@
 # Task 1 :
 
 # Find length of the longest substring with no duplicate characters.
+
+def longest_unique(s):
+    left = 0
+    seen = set()
+    max_len = 0
+
+    for right in range(len(s)):
+
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+
+        seen.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+
+print(longest_unique("abcabcbb"))
