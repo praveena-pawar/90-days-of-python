8ea3adb4d57ca143f9d23d8ce3ff616c16d6cfbe
praveena-pawar <praveena.dev02@gmail.com>
2026-02-05 15:30:34 +0530

    solved task 1

--- day35_sliding_window_minimum_average.py
+++ day35_sliding_window_minimum_average.py
@@ -4,4 +4,21 @@
 # nums = [3, 7, 5, 20, -10, 0, 12]
 # k = 2
 
+def minimum_average_of_subbarray(nums, k):
+    window_sum = sum(nums[:k])
+    minimum_average = window_sum / k
 
+    for i in range(k, len(nums)):
+        window_sum += nums[i]
+        window_sum -= nums[i - k]
+
+        current_avarge = window_sum / k
+        if current_avarge < minimum_average:
+            minimum_average = current_avarge
+
+    return minimum_average
+
+print(minimum_average_of_subbarray( [3, 7, 5, 20, -10, 0, 12], 2))
+
+#output:
+#-5.0
