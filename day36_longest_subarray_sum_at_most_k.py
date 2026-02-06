652dd3b3a4c1e0bedbfeee4679d8cec118654b42
praveena-pawar <praveena.dev02@gmail.com>
2026-02-06 12:33:33 +0530

    solved Task 1

--- day36_longest_subarray_sum_at_most_k.py
+++ day36_longest_subarray_sum_at_most_k.py
@@ -1,3 +1,24 @@
 # Task 1 :
 
 # Find the length of the longest subarray with sum ≤ k
+
+def longest_subarray_witgh_sum(nums, k):
+    left = 0
+    window_sum = 0
+    max_length = 0
+
+    for right in range(len(nums)):
+        window_sum += nums[right]
+
+        while window_sum > k:
+            window_sum -= nums[left]
+            left += 1
+
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
+
+print(longest_subarray_witgh_sum([2, 1, 1, 3, 2, 1],4))
+
+
+
