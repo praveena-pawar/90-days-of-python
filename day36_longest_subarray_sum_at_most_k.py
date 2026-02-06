325d44d52517f81ed1c0c5237f00121d9f40a301
praveena-pawar <praveena.dev02@gmail.com>
2026-02-06 23:38:45 +0530

    solved task 2

--- day36_longest_subarray_sum_at_most_k.py
+++ day36_longest_subarray_sum_at_most_k.py
@@ -30,5 +30,30 @@ print(longest_subarray_witgh_sum([2, 1, 1, 3, 2, 1],4))
 
 # Find the length of the longest subarray with at most k distinct numbers
 
+def longest_subarray_at_most_k_distinct(nums, k):
+    left = 0
+    freq = {}
+    max_length = 0
+
+    for right in range(len(nums)):
+        # add right element
+        freq[nums[right]] = freq.get(nums[right], 0) + 1
+
+        # shrink window if distinct count > k
+        while len(freq) > k:
+            freq[nums[left]] -= 1
+            if freq[nums[left]] == 0:
+                del freq[nums[left]]
+            left += 1
+
+        # update answer
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
+
+
+print(longest_subarray_at_most_k_distinct([1, 2, 1, 2, 3], 2))
+
+
 
 
