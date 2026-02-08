81fd951ac41cfe49eb92c98b38a4741cd9ffebcd
praveena-pawar <praveena.dev02@gmail.com>
2026-02-08 20:59:23 +0530

    solved problem 1

--- day37_count_even_sum_subarrays_fixed_window.py
+++ day37_count_even_sum_subarrays_fixed_window.py
@@ -2,3 +2,22 @@
 
 # Count subarrays of size k whose sum is even
 
+def even_sum(nums, k):
+    window_sum = sum(nums[:k])
+    even_sum = 0
+
+    if window_sum % 2 == 0:
+        even_sum += 1
+
+    for i in range(k, len(nums)):
+        window_sum += nums[i]      
+        window_sum -= nums[i - k] 
+
+        if window_sum % 2 == 0:
+            even_sum += 1
+            
+    return even_sum
+
+print(even_sum([2, 1, 5, 1, 3, 2], 3))
+
+
