acc59e736b2af577457128c2a29337f1d8791da2
praveena-pawar <praveena.dev02@gmail.com>
2026-02-09 15:20:54 +0530

    solved probelm 1

--- dsa/sliding_window/variable_window/day38_longest_subarray_sum_at_most_k.py
+++ dsa/sliding_window/variable_window/day38_longest_subarray_sum_at_most_k.py
@@ -1,3 +1,21 @@
 # problem 1 :
 
 # Longest subarray with sum ≤ k
+
+def longest_subarray(nums, k):
+    left = 0
+    window_sum = 0
+    max_len = 0
+
+    for right in range(len(nums)):
+        window_sum += nums[right]
+
+        while window_sum > k:
+            window_sum -= nums[left]
+            left += 1
+
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+print(longest_subarray([2, 1, 5, 2, 3], 7))
