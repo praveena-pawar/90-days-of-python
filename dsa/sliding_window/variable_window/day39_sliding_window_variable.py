a43250b1bd43926fc89429a0ed168193ee8dd36e
praveena-pawar <praveena.dev02@gmail.com>
2026-02-10 13:07:30 +0530

    solved problem 1

--- dsa/sliding_window/variable_window/day39_sliding_window_variable.py
+++ dsa/sliding_window/variable_window/day39_sliding_window_variable.py
@@ -1,3 +1,23 @@
 # problem 1 :
 
 # Longest subarray with at most k zeros
+
+def longest_suabarray_at_most_k_zeros(nums, k):
+    left = 0
+    zero_count = 0
+    max_len = 0
+
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zero_count += 1
+        
+        while zero_count > k:
+            if nums[left] == 0:
+                zero_count -= 1
+            left += 1
+
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+print(longest_suabarray_at_most_k_zeros( [1, 1, 0, 0, 1, 1, 0, 1], 2))
