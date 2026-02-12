d3216c8a05c753c8df2f24e70b80ab1ec296680c
praveena-pawar <praveena.dev02@gmail.com>
2026-02-12 23:54:57 +0530

    solved problem 2

--- dsa/prefix_sum/day41_Count Subarrays with Equal Number.py
+++ dsa/prefix_sum/day41_Count Subarrays with Equal Number.py
@@ -35,3 +35,28 @@ print(count_subarray_with_equal_number([0,1,0,1]))
 # problem 2 :
 
 # Count Subarrays Divisible by K
+
+def count_subarrays_divisible_by_k(nums, k):
+    remainder_count = {0: 1}   
+    prefix_sum = 0
+    count = 0
+
+    for num in nums:
+        prefix_sum += num
+
+        remainder = prefix_sum % k
+
+        remainder = (remainder + k) % k
+
+        if remainder in remainder_count:
+            count += remainder_count[remainder]
+            
+
+        remainder_count[remainder] = remainder_count.get(remainder, 0) + 1
+
+    return count
+
+
+nums = [4,5,0,-2,-3,1]
+k = 5
+print(count_subarrays_divisible_by_k(nums, k))
