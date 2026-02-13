5b31fa248f7ee34168e79cb37c1f10f02520c13f
praveena-pawar <praveena.dev02@gmail.com>
2026-02-13 13:07:12 +0530

    solved probelm 1

--- dsa/two_pointers/day42_Two_Sum.py
+++ dsa/two_pointers/day42_Two_Sum.py
@@ -1,3 +1,26 @@
 # problem 1 :
 
 # Two Sum (Sorted Array)
+
+def two_sum_sorted_array(nums, target):
+    left = 0
+    right = len(nums) - 1
+    current_sum = 0
+
+    while left < right:     
+        current_sum = nums[left] + nums[right]
+
+        if current_sum == target:
+            return left, right
+        
+        elif current_sum < target:
+            left += 1
+
+        else:
+            right -= 1
+
+
+    return current_sum
+
+print(two_sum_sorted_array( [1, 2, 3, 4, 6],6))
+
