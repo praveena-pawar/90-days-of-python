dbb8656a0e92f09eeb3f7f70b7a7abfbd9d1a0a0
praveena-pawar <praveena.dev02@gmail.com>
2026-02-14 23:03:56 +0530

    solved problem 1

--- dsa/two_pointers/day43_Check_if_Array_is_Palindrome.py
+++ dsa/two_pointers/day43_Check_if_Array_is_Palindrome.py
@@ -1,3 +1,17 @@
 # problem 1 :
 
 # Check if Array is Palindrome
+
+def is_palindrome(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        if arr[left] != arr[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+print(is_palindrome([1, 2, 3, 2, 1]))
