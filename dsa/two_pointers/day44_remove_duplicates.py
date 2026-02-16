aa34e111b781d20ac06a01d0be69776d7ae82a4e
praveena-pawar <praveena.dev02@gmail.com>
2026-02-16 16:02:29 +0530

    solved problem 2

--- dsa/two_pointers/day44_remove_duplicates.py
+++ dsa/two_pointers/day44_remove_duplicates.py
@@ -29,5 +29,21 @@ print(nums[:length])
 
 # Move all zeros to the end
 
+def move_zeros(nums):
+    write = 0
+
+    for read in range(len(nums)):
+        if nums[read] != 0:
+
+            nums[write] = nums[read]
+            write += 1
+
+    for i in range(write, len(nums)):
+        nums[i] = 0
+
+    return nums
+
+nums = [0, 1, 0, 3, 12]
+print(move_zeros(nums))
 
 
