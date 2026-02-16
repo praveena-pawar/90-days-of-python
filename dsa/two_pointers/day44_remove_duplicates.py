d72d9386d068614977600096e3182321b0f850c7
praveena-pawar <praveena.dev02@gmail.com>
2026-02-16 15:37:31 +0530

    solved problem 1

--- dsa/two_pointers/day44_remove_duplicates.py
+++ dsa/two_pointers/day44_remove_duplicates.py
@@ -1,3 +1,21 @@
 # problem 1 :
 
 # Remove Duplicates
+
+def remove_duplicates(nums):
+    write = 0
+
+    for read in range(1, len(nums)):
+        if nums[read] != nums[write]:
+            write += 1
+
+            nums[write] = nums[read]
+
+    return write + 1
+
+
+nums = [1, 1, 2, 2, 3]
+length = remove_duplicates(nums)
+print(nums[:length])
+
+
