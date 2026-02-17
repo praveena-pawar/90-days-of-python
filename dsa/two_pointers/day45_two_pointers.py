8791289628db699b263f056c5746f374777af6c1
praveena-pawar <praveena.dev02@gmail.com>
2026-02-17 11:52:35 +0530

    solved probelm 2

--- dsa/two_pointers/day45_two_pointers.py
+++ dsa/two_pointers/day45_two_pointers.py
@@ -35,3 +35,28 @@ print(is_palindrome("A man, a plan, a canal: Panama"))
 # problem 2 :
 
 # Container With Most Water
+
+height = [1,8,6,2,5,4,8,3,7]
+
+
+def container_with_most_water(height):
+   left = 0
+   right = len(height) - 1
+   max_area = 0
+
+   while left < right:
+      area = min(height[left], height[right]) * (right - left)
+     
+      max_area = max(max_area, area)
+
+      if height[left] < height[right]:
+         left += 1
+
+      else:
+         right -= 1
+
+   return max_area 
+
+print(container_with_most_water(height))
+      
+      
