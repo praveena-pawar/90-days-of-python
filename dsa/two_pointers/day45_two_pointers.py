ef2cdbb2d81864e898af72b2b2e916815e00a306
praveena-pawar <praveena.dev02@gmail.com>
2026-02-17 11:24:53 +0530

    solved probelm 1

--- dsa/two_pointers/day45_two_pointers.py
+++ dsa/two_pointers/day45_two_pointers.py
@@ -1,3 +1,26 @@
-# Problem —
+# Problem 1 :
 
 # Valid Palindrome
+
+def is_palindrome(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if not s[left].isalnum():
+          left += 1
+          continue
+        
+        if not s[right].isalnum():
+          right -= 1
+          continue
+
+
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+print(is_palindrome("A man, a plan, a canal: Panama"))
