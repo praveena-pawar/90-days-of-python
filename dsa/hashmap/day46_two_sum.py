fbe6178932dadb01bca99e6cfb3e13f3c6611c4a
praveena-pawar <praveena.dev02@gmail.com>
2026-02-18 23:33:38 +0530

    problem 2

--- dsa/hashmap/day46_two_sum.py
+++ dsa/hashmap/day46_two_sum.py
@@ -13,7 +13,17 @@ def two_sum(nums, target):
             return [seen[needed], i]
 
         seen[num] = i
+
 print(two_sum([2, 7, 11, 15], 9))
 
 #output:
 #[0, 1]   #these are index values 
+
+
+
+
+# problem 2 :
+
+# Return True if the array contains duplicates.
+# Return False if all numbers are unique.
+
