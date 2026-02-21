5d0c24be541b3e21ad4267760fc1fdabfff45f73
praveena-pawar <praveena.dev02@gmail.com>
2026-02-21 22:30:54 +0530

    solved problem 1

--- dsa/hashmap/day47_valid_anagram.py
+++ dsa/hashmap/day47_valid_anagram.py
@@ -1,3 +1,25 @@
 # problem 1 :
 
 # Given two strings s and t, return True if t is an anagram of s.
+
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+
+    for char in t:
+        if char not in count:
+            return False
+        count[char] -= 1
+        if count[char] < 0:
+            return False
+
+    return True
+
+
+print(is_anagram("listen", "silent"))  
+print(is_anagram("rat", "car"))        
