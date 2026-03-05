eef38b813f836a3ec33782427c5b4a39636cc593
praveena-pawar <praveena.dev02@gmail.com>
2026-03-05 23:00:19 +0530

    solved problem 1

--- dsa/stack/day48_next_greater_element_right.py
+++ dsa/stack/day48_next_greater_element_right.py
@@ -1,3 +1,21 @@
 # problem 1 :
 
 # Next Greater Element to the Right
+
+def next_greater(arr):
+    stack = []
+    result = [-1] * len(arr)
+
+    for i in range(len(arr)):
+        
+        while stack and arr[i] > arr[stack[-1]]:
+            index = stack.pop()
+            result[index] = arr[i]
+
+        stack.append(i)
+
+    return result
+
+
+arr = [2, 1, 2, 4, 3]
+print(next_greater(arr))
