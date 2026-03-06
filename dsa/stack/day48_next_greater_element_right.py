ea2ecbafdeecc08cf34bdb7a7701ae735d38b0f7
praveena-pawar <praveena.dev02@gmail.com>
2026-03-06 06:05:32 +0530

    solved problem 2

--- dsa/stack/day48_next_greater_element_right.py
+++ dsa/stack/day48_next_greater_element_right.py
@@ -29,3 +29,21 @@ print(next_greater(arr))
 # problem 2 :
 
 # Next Smaller Element to the Right
+
+def next_smaller(arr):
+    stack = []
+    result = [-1] * len(arr)
+
+    for i in range(len(arr)):
+        
+        while stack and arr[i] < arr[stack[-1]]:
+            index = stack.pop()
+            result[index] = arr[i]
+
+        stack.append(i)
+
+    return result
+
+
+arr = [4, 8, 5, 2, 25]
+print(next_smaller(arr))
