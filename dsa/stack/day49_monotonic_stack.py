a88adfe8faadf17ce70fadee9424a19aa09a3140
praveena-pawar <praveena.dev02@gmail.com>
2026-03-15 10:39:16 +0530

    solved problem 2

--- dsa/stack/day49_monotonic_stack.py
+++ dsa/stack/day49_monotonic_stack.py
@@ -30,3 +30,21 @@ print(next_smaller_left(arr))
 
 # problem 3 :
 # Next Greater Element to the Left
+arr = [2, 1, 2, 4, 3]
+
+def next_greater_left(arr):
+    stack = []
+    result = [-1] * len(arr)
+
+    for i in range(len(arr)):
+        while stack and arr[stack[-1]] <= arr[i]:
+            stack.pop()
+
+        if stack:
+            result[i] = arr[stack[-1]]
+        
+        stack.append(i)
+
+    return result
+
+print(next_greater_left(arr))
