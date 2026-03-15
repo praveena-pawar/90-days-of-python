deee1631b2c50a4361cab5835acf778ebad635f2
praveena-pawar <praveena.dev02@gmail.com>
2026-03-15 10:33:12 +0530

    solved problem 1

--- dsa/stack/day49_monotonic_stack.py
+++ dsa/stack/day49_monotonic_stack.py
@@ -1,2 +1,22 @@
 # problem 1 :
 # Next Smaller Element to the Left
+
+arr = [4, 5, 2, 10, 8]
+
+def next_smaller_left(arr):
+    stack = []
+    result = [-1] * len(arr)
+
+    for i in range(len(arr)):
+        while stack and arr[stack[-1]] >= arr[i]:
+            stack.pop()
+
+        if stack:
+            result[i] = arr[stack[-1]]
+
+        stack.append(i)
+
+    return result
+
+print(next_smaller_left(arr))
+
