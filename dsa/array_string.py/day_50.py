d1b8f9c64720269d6175a25b710701809b5e2414
praveena-pawar <praveena.dev02@gmail.com>
2026-07-10 12:33:07 +0530

    solved problem 2

--- dsa/array_string.py/day_50.py
+++ dsa/array_string.py/day_50.py
@@ -21,3 +21,19 @@ print(two_sum(nums, target))
 
 
 # 2: Best Time to Buy and Sell Stock
+def maxProfit(prices):
+    minimum_price = prices[0]
+    maximum_profit = 0
+
+    for price in prices[1:]:
+        if price < minimum_price:
+            minimum_price = price
+        else:
+            profit = price - minimum_price
+            if profit > maximum_profit:
+                maximum_profit = profit
+
+    return maximum_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit(prices))
