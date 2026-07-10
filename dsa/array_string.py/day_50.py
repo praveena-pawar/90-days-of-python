1c4879e71ca070f4de3ff323d9fe3fbc221ff154
praveena-pawar <praveena.dev02@gmail.com>
2026-07-10 12:11:17 +0530

    solved problem 1

--- dsa/array_string.py/day_50.py
+++ dsa/array_string.py/day_50.py
@@ -1,1 +1,19 @@
 # 1: Two Sum
+def two_sum(nums, target):
+    seen = {}
+    num = 0
+    needed = 0
+
+    for i in range(len(nums)):
+        num = nums[i]
+        needed = target - num
+
+        if needed in seen:
+            return [seen[needed], i]
+        
+        seen[num] = i
+
+
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))
