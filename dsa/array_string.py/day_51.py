13db7445acec522ceb5101cf905fc35b67b8ee46
praveena-pawar <praveena.dev02@gmail.com>
2026-07-11 10:55:47 +0530

    solved problem 1

--- dsa/array_string.py/day_51.py
+++ dsa/array_string.py/day_51.py
@@ -1,1 +1,12 @@
 # 1: Find the Largest Element
+def largest_element(nums):
+    large_num = float('-inf')
+
+    for num in nums:
+        if num > large_num:
+            large_num = num
+
+    return large_num
+
+nums = [5, 2, 9, 1, 7]
+print(largest_element(nums))
