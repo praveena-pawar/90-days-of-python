500c0ab7ab42b5d9887c5cb93b0d7a8b6f1d52db
praveena-pawar <praveena.dev02@gmail.com>
2026-07-11 11:51:35 +0530

    solved problem 4

--- dsa/array_string.py/day_51.py
+++ dsa/array_string.py/day_51.py
@@ -50,3 +50,17 @@ print(array_sorted_or_not(nums))
 
 
 # 4: Reverse an Array (In-Place)
+def reverse_an_array(nums):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+
+            left += 1
+            right -= 1
+
+    return nums
+
+nums = [1, 2, 3, 4, 5]
+print(reverse_an_array(nums))
