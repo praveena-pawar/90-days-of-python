2e17dd14b1d7f6b92ce2b90ee9cce7f8044f8b0e
praveena-pawar <praveena.dev02@gmail.com>
2026-07-16 21:19:09 +0530

    solved problem 5

--- dsa/array_string.py/day_52.py
+++ dsa/array_string.py/day_52.py
@@ -73,3 +73,29 @@ print(frequency(nums))
 
 
 # 5: Check if Two Strings are Anagrams
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    freq1 = {}
+    freq2 = {}
+
+    for num in s:
+        if num in freq1:
+            freq1[num] += 1
+        else:
+            freq1[num] = 1
+
+    for num in t:
+        if num in freq2:
+            freq2[num] += 1
+        else:
+            freq2[num] = 1
+
+    return freq1 == freq2
+
+
+s = "listen"
+t = "silent"
+
+print(is_anagram(s, t))
