9e9009f797ab2b872f9be4a129f9f6c9653da25b
praveena-pawar <praveena.dev02@gmail.com>
2026-07-16 21:03:44 +0530

    solved problem 2

--- dsa/array_string.py/day_52.py
+++ dsa/array_string.py/day_52.py
@@ -14,3 +14,21 @@ print(missing_number(nums))
 
 
 # 2: Find the Majority Element
+def majority_element(nums):
+    candidate = None
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+
+    return candidate
+
+
+nums = [2, 2, 1, 1, 1, 2, 2]
+print(majority_element(nums))
