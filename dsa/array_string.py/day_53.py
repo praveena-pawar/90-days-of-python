ac141c8efce5bbfcd746fddaa97ba19c0ee84587
praveena-pawar <praveena.dev02@gmail.com>
2026-07-18 21:57:44 +0530

    solved problem 1

--- dsa/array_string.py/day_53.py
+++ dsa/array_string.py/day_53.py
@@ -1,1 +1,21 @@
 # 1: Longest Common Prefix
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+
+    prefix = ""
+
+    for i in range(len(strs[0])):
+
+        for word in strs[1:]:
+
+            if i >= len(word) or word[i] != strs[0][i]:
+                return prefix
+
+        prefix += strs[0][i]
+
+    return prefix
+
+
+strs = ["flower", "flow", "flight"]
+print(longest_common_prefix(strs))
