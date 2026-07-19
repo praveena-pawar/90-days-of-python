9d9184c39fbe3fb1de4334e7ce95c0e82bec3899
praveena-pawar <praveena.dev02@gmail.com>
2026-07-19 21:55:42 +0530

    solved problem 2

--- dsa/array_string.py/day_54.py
+++ dsa/array_string.py/day_54.py
@@ -20,3 +20,23 @@ print(is_subsequence(s, t))
 
 
 # 2: Find the First Unique Character in a String
+def first_unique_char(s):
+    freq = {}
+
+    # Count frequency
+    for ch in s:
+        if ch in freq:
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+
+    # Find first unique character
+    for i in range(len(s)):
+        if freq[s[i]] == 1:
+            return i
+
+    return -1
+
+
+s = "loveleetcode"
+print(first_unique_char(s))
