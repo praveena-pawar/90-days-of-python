d04a66439dd3afa6131d25f8be530e68d849e3fa
praveena-pawar <praveena.dev02@gmail.com>
2026-07-20 21:01:03 +0530

    solved problem 2

--- dsa/two_pointers/day_55.py
+++ dsa/two_pointers/day_55.py
@@ -18,3 +18,19 @@ print(revesre_string(s))
 
 
 # 2: Valid Palindrome (LeetCode 125)
+def valid_palindrome(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        
+        left += 1
+        right -= 1
+
+
+    return True
+
+s = "madam"
+print(valid_palindrome(s))
