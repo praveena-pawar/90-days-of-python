844e6ab8020a3aeb89e5e80cd676bcd944b3b95b
praveena-pawar <praveena.dev02@gmail.com>
2026-07-20 21:16:56 +0530

    solved problem 3

--- dsa/two_pointers/day_55.py
+++ dsa/two_pointers/day_55.py
@@ -39,3 +39,25 @@ print(valid_palindrome(s))
 
 
 # 3: Two Sum II – Input Array Is Sorted (LeetCode 167)
+def two_sum(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+
+    while left < right:
+        current_sum = numbers[left] + numbers[right]
+
+        if current_sum == target:
+            return left, right 
+        
+        elif current_sum > target:
+            right -= 1
+
+        else:
+            left += 1  
+        
+    return None
+
+
+numbers = [2, 7, 11, 15]
+target = 9
+print(two_sum(numbers, target))
