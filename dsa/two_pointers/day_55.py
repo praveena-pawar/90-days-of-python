0f38fd1adf8b3d26f1ed197c08186b6ee23bde63
praveena-pawar <praveena.dev02@gmail.com>
2026-07-20 20:39:13 +0530

    solved problem 1

--- dsa/two_pointers/day_55.py
+++ dsa/two_pointers/day_55.py
@@ -1,1 +1,16 @@
 # 1: Reverse String (LeetCode 344)
+def revesre_string(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+
+        left += 1
+        right -= 1
+
+    return s
+
+s = ["h", "e", "l", "l", "o"]
+
+print(revesre_string(s))
