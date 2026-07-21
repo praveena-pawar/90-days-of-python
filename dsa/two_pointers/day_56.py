8ee6a807b1911c647ae35b7932f84d90d2595eb9
praveena-pawar <praveena.dev02@gmail.com>
2026-07-21 22:16:49 +0530

    solved problem 4

--- dsa/two_pointers/day_56.py
+++ dsa/two_pointers/day_56.py
@@ -1,1 +1,15 @@
 # 4: Remove Duplicates from Sorted Array (LeetCode 26)
+def remove_duplicates(nums):
+    slow = 0
+
+    for fast in range(1, len(nums)):
+        if nums[slow] != nums[fast]:
+            slow += 1
+            nums[slow] = nums[fast]
+
+
+    return slow + 1
+
+nums = [1, 1, 2]
+print(remove_duplicates(nums))
+
