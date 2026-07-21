d359f8939106d4324545972c4ab71f24f3f88de2
praveena-pawar <praveena.dev02@gmail.com>
2026-07-21 22:30:36 +0530

    solved problem 5

--- dsa/two_pointers/day_56.py
+++ dsa/two_pointers/day_56.py
@@ -17,3 +17,21 @@ print(remove_duplicates(nums))
 
 
 # 5: Move Zeroes (LeetCode 283)
+def move_zeros(nums):
+    slow = 0
+
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[slow] = nums[fast]
+            slow += 1
+
+        
+    while slow < len(nums):
+        nums[slow] = 0
+        slow += 1
+   
+
+    return nums
+
+nums = [0, 1, 0, 3, 12]
+print(move_zeros(nums))
