eaf215434907ca777db1dd84a9ba891c38975f3c
praveena-pawar <praveena.dev02@gmail.com>
2026-07-24 21:52:18 +0530

    solved problem 1

--- dsa/two_pointers/day_57.py
+++ dsa/two_pointers/day_57.py
@@ -1,1 +1,19 @@
 # 1: Remove Element (LeetCode 27)
+def remove_element(nums, val):
+    slow = 0
+
+    for fast in range(len(nums)):
+        if nums[fast] != val:
+            nums[slow] = nums[fast]
+            slow += 1
+
+    return slow
+
+
+nums = [3, 2, 2, 3]
+val = 3
+
+k = remove_element(nums, val)
+
+print(k)          
+print(nums[:k])  
