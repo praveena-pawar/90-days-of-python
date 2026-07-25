5bde9c69c87d7f3beec93d8a11b7ae20a5bb859b
praveena-pawar <praveena.dev02@gmail.com>
2026-07-25 22:18:18 +0530

    solved problem

--- dsa/two_pointers/day_58.py
+++ dsa/two_pointers/day_58.py
@@ -1,1 +1,25 @@
 # Container With Most Water (LeetCode 11)
+def max_area(height):
+    left = 0
+    right = len(height) - 1
+
+    max_water = 0
+
+    while left < right:
+
+        width = right - left
+        current_height = min(height[left], height[right])
+        area = width * current_height
+
+        max_water = max(max_water, area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_water 
+
+
+height = [1,8,6,2,5,4,8,3,7]
+print(max_area(height))
